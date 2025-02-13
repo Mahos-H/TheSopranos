746a45ad72cf0b3433b08603067d3d34f1013734
sortira <aritroshomework@gmail.com>
2025-02-13 18:29:44 +0530

    audio input feature for speech to text implemented

--- funcs/speech_to_text.py
+++ funcs/speech_to_text.py
@@ -1,13 +1,12 @@
 import streamlit as st
+from st_audiorec import st_audiorec
 
 def app():
     st.title('Speech to Text')
     
     st.write('Use your microphone to convert speech to text.')
     
-    # Use microphone
-    use_microphone = st.checkbox('Use Microphone')
-    
-    if use_microphone:
-        st.write("Microphone functionality is not yet implemented.")
-        # Capture and process audio for speech to text
+    wav_audio_data = st_audiorec()
+
+    if wav_audio_data is not None:
+        st.audio(wav_audio_data, format='audio/wav')    
